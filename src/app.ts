488167507e168bc4bc8a06e1d2b4e531871b3870
avrhahm sason <avi6190i@gmail.com>
2025-09-13 23:32:33 +0300

    v++

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import mainRouter from "./main_router";
 import { startServer } from "./helpers";
 import { getAllDocuments } from "./firebase/helpers";
-import dotenv from "dotenv";
-dotenv.config();
+
 const init = async () => {
     await startServer(mainRouter);
     const users = await getAllDocuments("users");
